24315cd3724cc17c5cd3092473caf1c17a9e2722
Zenkazio <zenkazio@proton.me>
2025-06-29 14:07:02 +0200

    So 29. Jun 14:07:02 CEST 2025

--- src/git.rs
+++ src/git.rs
@@ -8,32 +8,28 @@ pub fn git_add_all(path: &PathBuf) {
         .unwrap();
 }
 pub fn git_commit_with_date(path: &PathBuf) {
-    dbg!(
-        Command::new("git")
-            .args(["commit", "-m", "$(date)"])
-            .current_dir(path)
-            .status()
-            .unwrap()
-    );
+    let date = Command::new("date").output().unwrap();
+    let msg = String::from_utf8_lossy(&date.stdout).trim().to_string();
+
+    Command::new("git")
+        .args(["commit", "-m", &msg])
+        .current_dir(path)
+        .status()
+        .unwrap();
 }
 
 #[cfg(test)]
 mod tests {
     use std::{path::PathBuf, process::Command};
 
-    use crate::git::git_commit_with_date;
+    use crate::git::{git_add_all, git_commit_with_date};
 
     #[test]
     fn test_commands() {
         dbg!(Command::new("echo").arg("hallo").status().unwrap());
 
-        dbg!(
-            Command::new("git")
-                .args(["add", "."])
-                .current_dir("/home/zenkazio/Projects/bloi/")
-                .status()
-                .unwrap()
-        );
-        git_commit_with_date(&PathBuf::from("/home/zenkazio/Projects/bloi/"));
+        let base = PathBuf::from("/home/zenkazio/Projects/bloi/");
+        git_add_all(&base);
+        git_commit_with_date(&base);
     }
 }
